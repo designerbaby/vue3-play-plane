1ef484693ca7e7658c5202caa59138f755c2aaa5
liujiana <liujiana@yy.com>
2020-08-20 11:23:32 +0800

    feat: save map

---
+++ src/components/Map.js
@@ -0,0 +1,7 @@
+import { game } from '../Game'
+import { h, ref, defineComponent, onMounted, onUnmounted } from '@vue/runtime-core'
+import { stage } from '../config/stage'
+import mapImg from '../resource/assets/map.jpg'
+
+
+
